eb8b7382e2c95e44cc75e8cfd9be437204f9d8db
“jingpeng <1753703407@qq.com>
2025-01-10 22:35:05 +0800

    解决日志类bug，保证跨天后日志会自动写到当天的文件，而不是等300mb文件写满后轮转

--- utils/custom_log_handler.py
+++ utils/custom_log_handler.py
@@ -6,6 +6,7 @@ class CustomRotatingFileHandler(RotatingFileHandler):
     def __init__(self, base_name, log_dir, *args, **kwargs):
         self.base_name = base_name
         self.log_dir = log_dir
+        self.current_date = datetime.now().strftime('%Y-%m-%d')  # 记录当前日期
         super().__init__(self._get_log_filename(), *args, **kwargs)
 
     def _get_log_filename(self):
@@ -18,3 +19,11 @@ class CustomRotatingFileHandler(RotatingFileHandler):
         self.stream.close()
         self.baseFilename = self._get_log_filename()
         self.stream = self._open()
+
+    def emit(self, record):
+        """每次写日志时检查日期是否变化"""
+        new_date = datetime.now().strftime('%Y-%m-%d')
+        if new_date != self.current_date:  # 如果日期变化，强制更新日志文件
+            self.current_date = new_date
+            self.doRollover()
+        super().emit(record)
